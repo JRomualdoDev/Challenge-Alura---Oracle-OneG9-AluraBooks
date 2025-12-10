0c1e5ba9782a45d04e646a688b633114dcf0f9f6
JoseRomualdo <jromualdo3@hotmail.com>
2025-12-10 14:20:16 -0300

    Initial commit

---
+++ src/test/java/com/AluraOracleOneG9/Books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AluraOracleOneG9.Books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
