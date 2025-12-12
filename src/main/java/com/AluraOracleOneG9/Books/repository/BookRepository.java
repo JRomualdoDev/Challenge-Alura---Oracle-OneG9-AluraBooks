7ec84e967e189cd1e329dd6865c70e4d55b48def
JoseRomualdo <jromualdo3@hotmail.com>
2025-12-12 08:08:30 -0300

    feat(BookRepository): new file.

---
+++ src/main/java/com/AluraOracleOneG9/Books/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.AluraOracleOneG9.Books.repository;
+
+import com.AluraOracleOneG9.Books.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    Optional<Book> findByTitle(String title);
+}
