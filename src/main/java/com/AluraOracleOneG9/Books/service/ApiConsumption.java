2564c03f3ae1fddc3579303f73dcf4a4ef3b1d64
JoseRomualdo <jromualdo3@hotmail.com>
2025-12-12 08:05:09 -0300

    feat(ApiConsumption): new file.

---
+++ src/main/java/com/AluraOracleOneG9/Books/service/ApiConsumption.java
@@ -0,0 +1,32 @@
+package com.AluraOracleOneG9.Books.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiConsumption {
+
+    public String getData(String address) {
+        HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address))
+                .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Status code: " + response.statusCode());
+        System.out.println("Resposta recebida:");
+
+        String json = response.body();
+        return json;
+    }
+}
