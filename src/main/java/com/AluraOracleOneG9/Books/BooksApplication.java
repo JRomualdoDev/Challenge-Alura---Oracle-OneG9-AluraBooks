4001db4c658ad37fd037be8503a1043e6e031db5
JoseRomualdo <jromualdo3@hotmail.com>
2025-12-12 08:09:12 -0300

    refactor(BooksApplication): implements commandLineRunner and index class.

--- src/main/java/com/AluraOracleOneG9/Books/BooksApplication.java
+++ src/main/java/com/AluraOracleOneG9/Books/BooksApplication.java
@@ -1,13 +1,31 @@
 package com.AluraOracleOneG9.Books;
 
+import com.AluraOracleOneG9.Books.index.Index;
+import com.AluraOracleOneG9.Books.repository.AuthorRepository;
+import com.AluraOracleOneG9.Books.service.BookService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BooksApplication {
+public class BooksApplication implements CommandLineRunner {
+
+    private BookService bookService;
+    private AuthorRepository authorRepository;
+
+    public BooksApplication(BookService bookService,  AuthorRepository authorRepository) {
+        this.bookService = bookService;
+        this.authorRepository = authorRepository;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Index index = new Index(bookService,  authorRepository);
+        index.showMenu();
+    }
 }
