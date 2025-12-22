085efdd73945697f8435384229e1c93e4453b927
JoseRomualdo <jromualdo3@hotmail.com>
2025-12-22 19:54:46 -0300

    refactor(BookService): add getBook and getBookSpecificLanguage.

--- src/main/java/com/AluraOracleOneG9/Books/service/BookService.java
+++ src/main/java/com/AluraOracleOneG9/Books/service/BookService.java
@@ -1,14 +1,17 @@
 package com.AluraOracleOneG9.Books.service;
 
+import com.AluraOracleOneG9.Books.DTO.BookDTO;
 import com.AluraOracleOneG9.Books.model.ApiGutendexData;
 import com.AluraOracleOneG9.Books.model.Author;
 import com.AluraOracleOneG9.Books.model.Book;
 import com.AluraOracleOneG9.Books.repository.AuthorRepository;
 import com.AluraOracleOneG9.Books.repository.BookRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,14 +30,12 @@ public class BookService {
         try {
             bookApi.books().forEach(book -> {
 
-                Optional<Book> existBook = bookRepository.findByTitle(book.title());
-
-                System.out.println(existBook);
+                Optional<Book> existBook = bookRepository.findFirstByTitle(book.title());
 
                 if (existBook.isEmpty()) {
                     Set<Author> managedAuthors = book.authors().stream()
                             .map(aut -> {
-                                return authorRepository.findByName(aut.name())
+                                return authorRepository.findByNameContainingIgnoreCase(aut.name())
                                         .orElseGet(() -> {
                                             Author novo = new Author(
                                                     aut.name(),
@@ -49,10 +50,10 @@ public class BookService {
                     Book newBook = new Book();
                     newBook.setTitle(book.title());
                     newBook.setAuthors(managedAuthors);
-                    newBook.setSummaries(book.summaries());
-                    newBook.setSubjects(book.subjects());
-                    newBook.setBookshelves(book.bookshelves());
-                    newBook.setLanguages(book.languages());
+                    newBook.setSummaries(new HashSet<>(book.summaries()));
+                    newBook.setSubjects(new HashSet<>(book.subjects()));
+                    newBook.setBookshelves(new HashSet<>(book.bookshelves()));
+                    newBook.setLanguages(new HashSet<>(book.languages()));
                     newBook.setCopyright(book.copyright());
                     newBook.setFormats(book.formats());
 
@@ -64,5 +65,64 @@ public class BookService {
             ex.printStackTrace();
             return false;
         }
+    }
+
+    public List<Book> getBook(String title) {
+
+        return bookRepository.findByTitleContaining(title);
+
+    }
+
+    public List<BookDTO> getAllBooks(int numBooks) {
+
+        if (numBooks <= 0) return new ArrayList<>();;
+
+        List<BookDTO> books = bookRepository.findAll(PageRequest.of(0, numBooks))
+                .map(
+                        book -> new BookDTO(
+                                book.getTitle(),
+                                book.getSummaries(),
+                                book.getSubjects(),
+                                book.getBookshelves(),
+                                book.getLanguages(),
+                                book.isCopyright(),
+                                book.getMediaTypes(),
+                                book.getDownloadCount(),
+                                book.getFormats(),
+                                book.getAuthors()
+                                        .stream()
+                                        .map(Author::getName)
+                                        .collect(Collectors.toSet())
+                        )
+                ).toList();
+
+        return books;
+
+
+    }
+
+    public List<BookDTO> getBookSpecificLanguage(String language) {
+
+        List<BookDTO> bookDTOS = bookRepository.findAllByLanguages(language)
+                .stream()
+                .map(
+                        book -> new BookDTO(
+                                book.getTitle(),
+                                book.getSummaries(),
+                                book.getSubjects(),
+                                book.getBookshelves(),
+                                book.getLanguages(),
+                                book.isCopyright(),
+                                book.getMediaTypes(),
+                                book.getDownloadCount(),
+                                book.getFormats(),
+                                book.getAuthors()
+                                        .stream()
+                                        .map(Author::getName)
+                                        .collect(Collectors.toSet())
+                        )
+                ).toList();
+
+        return bookDTOS;
     }
 }
