5cec76b7948630021e4d23b02a8edd0352dbac25
JoseRomualdo <jromualdo3@hotmail.com>
2025-12-22 19:51:08 -0300

    refactor(AuthorRepository): add findAuthorAliveIn.

--- src/main/java/com/AluraOracleOneG9/Books/repository/AuthorRepository.java
+++ src/main/java/com/AluraOracleOneG9/Books/repository/AuthorRepository.java
@@ -2,11 +2,17 @@ package com.AluraOracleOneG9.Books.repository;
 
 import com.AluraOracleOneG9.Books.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-    Optional<Author> findByName(String name);
+    Optional<Author> findByNameContainingIgnoreCase(String name);
+
+    @Query("SELECT a FROM Author a WHERE :year BETWEEN a.birthYear AND a.deathYear")
+    List<Author> findAuthorAliveIn(@Param("year") int year);
 }
