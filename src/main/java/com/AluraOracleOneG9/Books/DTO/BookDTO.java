2a737998a9d3db4ffa2359bca80e06e9af399651
JoseRomualdo <jromualdo3@hotmail.com>
2025-12-22 21:23:51 -0300

    refactor(BookDTO): remove import unused.

--- src/main/java/com/AluraOracleOneG9/Books/DTO/BookDTO.java
+++ src/main/java/com/AluraOracleOneG9/Books/DTO/BookDTO.java
@@ -1,9 +1,5 @@
 package com.AluraOracleOneG9.Books.DTO;
 
-import com.AluraOracleOneG9.Books.model.Author;
-
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
